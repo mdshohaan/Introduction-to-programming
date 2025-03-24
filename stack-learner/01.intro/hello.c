4a2019f3999e7fcbe5efd6f627a1264ddb29a55a
mdshohaan <irfanulislamdu@gmail.com>
2025-03-24 09:39:29 +0600

    Chore:Add exploration C

--- stack-learner/01.intro/hello.c
+++ stack-learner/01.intro/hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(){
-  printf("Hello world\n"); 
+  printf("Hello world\n");  
   return 0;
 }
+// compiling
+// executing
---
+++ stack-learner/01.intro/print.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+  printf("Irfanul Islam");
+  return 0 ;
+}
