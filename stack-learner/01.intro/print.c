eab6eb855c82037fc74466c94789ffff6e48951f
mdshohaan <irfanulislamdu@gmail.com>
2025-03-24 10:13:49 +0600

    Chore:Add C syntax

--- stack-learner/01.intro/print.c
+++ stack-learner/01.intro/print.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-  printf("Irfanul Islam");
+  printf("Irfanul Islam\n");
+  printf("University of Dhaka\n");
+  printf("%d\n",10+12); 
+  printf("%d\n",10-12); 
   return 0 ;
-}
+} 
+//  A computer program is a list of "instructions" to be "executed" by a computer.
+
+// In a programming language, these programming instructions are called statements.
+
+// The following statement "instructs" the compiler to print the text "Hello World" to the screen:
