1ba76766bc376b31aeea722eba1dad450d3f6ffd
mdshohaan <irfanulislamdu@gmail.com>
2025-03-24 17:40:56 +0600

    feat:Real l;ife example of C var

--- stack-learn/01.intro/variable.c
+++ stack-learn/01.intro/variable.c
@@ -21,6 +21,16 @@ int main(){
     x = y = z = 50;
     printf("%d", x + y + z);
  
+     // Student data
+int studentID = 15;
+int studentAge = 23;
+float studentFee = 75.25;
+char studentGrade = 'B';
 
+// Print variables
+printf("Student id: %d\n", studentID);
+printf("Student age: %d\n", studentAge);
+printf("Student fee: %f\n", studentFee);
+printf("Student grade: %c", studentGrade);
    return 0;  
 };
