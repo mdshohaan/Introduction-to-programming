b777ef3795ed3e6bec9d9935e5cbe211e28d48b3
mdshohaan <irfanulislamdu@gmail.com>
2025-03-24 17:45:02 +0600

    feat:Add data types

---
+++ stack-learn/01.intro/data.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main({
+
+// Create variables
+int myNum = 5;             // Integer (whole number)
+float myFloatNum = 5.99;   // Floating point number
+char myLetter = 'D';       // Character
+
+  return 0
+})
