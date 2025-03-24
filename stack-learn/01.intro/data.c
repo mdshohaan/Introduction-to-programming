120f2de898b7d19dcd3c241db519584b45b688ba
mdshohaan <irfanulislamdu@gmail.com>
2025-03-24 17:46:59 +0600

    feat:Add data

--- stack-learn/01.intro/data.c
+++ stack-learn/01.intro/data.c
@@ -14,3 +14,11 @@ printf("%c\n", myLetter);
 
   return 0
 })
+
+
+
+
+// int	2 or 4 bytes	Stores whole numbers, without decimals	1
+// float	4 bytes	Stores fractional numbers, containing one or more decimals. Sufficient for storing 6-7 decimal digits	1.99
+// double	8 bytes	Stores fractional numbers, containing one or more decimals. Sufficient for storing 15 decimal digits	1.99
+// char	1 byte	Stores a single character/letter/number, or ASCII values	'A'
