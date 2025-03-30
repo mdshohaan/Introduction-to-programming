e6be795172adb0e2e25186f1f1b525e798fd6405
mdshohaan <irfanulislamdu@gmail.com>
2025-03-30 11:58:36 +0600

    feat:explore c

--- stack-learn/01.intro/print.c
+++ stack-learn/01.intro/print.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 int main(){
- int a ;
+ int a =10;
  scanf("%d",&a) ;
  printf("The Number is %d\n",a);
  printf("Hello world\n");
   return 0 ;
 } 
-//  A computer program is a list of "instructions" to be "executed" by a computer.
+/*
+  A computer program is a list of "instructions" to be "executed" by a computer.
+ 
+ In a programming language, these programming instructions are called statements.
 
-// In a programming language, these programming instructions are called statements.
-
-// The following statement "instructs" the compiler to print the text "Hello World" to the screen:
+The following statement "instructs" the compiler to print the text "Hello World" to the screen:
+*/
