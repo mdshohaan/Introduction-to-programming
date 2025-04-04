470ae3fb4367987b62da084e694b3c856fe5d458
mdshohaan <irfanulislamdu@gmail.com>
2025-04-04 16:39:01 +0600

    chore:Add scanf data

--- stack-learn/01.intro/input.c
+++ stack-learn/01.intro/input.c
@@ -3,7 +3,11 @@
 int main(){
 
    int a, b ;
-   scanf("%d",& a); 
-   printf("%d", a);
+   float c , d ;
+   scanf("%d",& a ); 
+   scanf("%d",& b ); 
+   printf("%d %d", a,b);  
+   scanf("%f %f",&c,&d);
+   printf("%f %f",c,d);
   return 0;
 }
