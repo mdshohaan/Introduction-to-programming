566cef3e48e7fe5d53c0476109bb51dfbb19dc60
mdshohaan <irfanulislamdu@gmail.com>
2025-04-05 23:19:48 +0600

    chore:input data

--- introduction-to-programming/module-1/first.c
+++ introduction-to-programming/module-1/first.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
-int main(){
-  printf("hello");
-  printf("world");
-  return 0; 
+#include<stdbool.h>
+int main() 
+{
+   int num1 = 19;
+
+   float num2 = 2.33;
+   printf("%.2f\n",num2); 
+   bool f = true; 
+   printf("%d",f);
+   return 0; 
 }
