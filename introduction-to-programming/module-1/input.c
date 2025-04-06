484f9fec3b6d6eb378810358ad2d5b6689102c32
mdshohaan <irfanulislamdu@gmail.com>
2025-04-06 12:05:29 +0600

    feat:long long int

--- introduction-to-programming/module-1/input.c
+++ introduction-to-programming/module-1/input.c
@@ -2,8 +2,10 @@
 
  int main()
  {
-    long long int a = 10000000000000;
-    printf("%lld",a);
+     long long int a = 10000000000000;
+     printf("%lld\n",a);
+     double f =1234.35784358;
+     printf("%lf",f);
   
     return 0;
  }  
