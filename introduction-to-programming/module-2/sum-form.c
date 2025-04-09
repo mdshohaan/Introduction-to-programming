c562977317586c6c06c1e976e7a79fa07b31dd34
mdshohaan <irfanulislamdu@gmail.com>
2025-04-10 00:54:35 +0600

    chore:Add break

--- introduction-to-programming/module-2/sum-form.c
+++ introduction-to-programming/module-2/sum-form.c
@@ -2,14 +2,27 @@
 
 int main()
 {
-   int a ;
-   scanf("%d\n",&a);
-   for (int i = 0; i <= a; i++)
-   {
-    printf("%d\n",i);
-   }
+  //  int a ;
+  //  scanf("%d\n",&a);
+  //  int sum = 0;
+  //  for (int i = 0; i <= a; i++)
+  //  {
+  //  sum += i ;
+  //  }
    
+  //  printf("%d",sum); 
+  int a = 14;
+  for (int i = 1; i <= a; i++)
+  {  
 
+   if(i == 5){
+    break;
+   }
+  printf("%d\n",i);
+
+
+  }
+  
 
   return 0 ;
 }
