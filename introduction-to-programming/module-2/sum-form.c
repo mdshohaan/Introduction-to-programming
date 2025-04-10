143e45257ae2384f1bf063c2b9ae4e4822c7ee1e
mdshohaan <irfanulislamdu@gmail.com>
2025-04-10 19:39:04 +0600

    chore:Add loop

--- introduction-to-programming/module-2/sum-form.c
+++ introduction-to-programming/module-2/sum-form.c
@@ -2,26 +2,17 @@
 
 int main()
 {
-  //  int a ;
-  //  scanf("%d\n",&a);
-  //  int sum = 0;
-  //  for (int i = 0; i <= a; i++)
-  //  {
-  //  sum += i ;
-  //  }
-   
-  //  printf("%d",sum); 
-  int a = 14;
-  for (int i = 1; i <= a; i++)
-  {  
 
-   if(i == 5){
-    break;
-   }
-  printf("%d\n",i);
+int counter ;
+for (counter  = 1; counter <= 5; counter++)
+{
+  printf("%d\n",counter); 
+}
+  printf("%d\n",counter);
 
 
-  }
+
+  
   
 
   return 0 ;
