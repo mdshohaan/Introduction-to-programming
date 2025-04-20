25bd17cb991b048586de3dd2ac73ffb31ef527dd
mdshohaan <irfanulislamdu@gmail.com>
2025-04-20 12:47:21 +0600

    chore:Add prblm

--- introduction-to-programming/pattern/pattern6.c
+++ introduction-to-programming/pattern/pattern6.c
@@ -3,12 +3,57 @@
 int main() {
   int n, row, col;
   scanf("%d", &n);
-  for (row = 1; row <= n; row++) {
+  for (row = n; row >= 1; row--) {
     for (col = 1; col <= row; col++) {
-      printf("%c ", col + 64);
+      // printf("%d ", col);
+      // printf("%d ", row);
+      // printf("%d ", row % 2);
+      // printf("%d ", col % 2);
+      printf("* ");
     }
     printf("\n");
   }
 
   return 0;
 }
+/* col
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+*/
+
+/* row
+5 5 5 5 5
+4 4 4 4
+3 3 3
+2 2
+1
+*/
+
+/*
+1 1 1 1 1
+0 0 0 0
+1 1 1
+0 0
+1
+*/
+
+/*
+1 0 1 0 1
+1 0 1 0
+1 0 1
+1 0
+1
+
+*/
+
+/*
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
